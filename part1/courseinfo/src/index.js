9b856bdd2dd9b1ff5d99e800902b2fe7fe9d3d42
Pablo Pérez Pérez <pablo_perez@posteo.net>
2020-04-14 03:26:57 +0100

    Fixed to only include part1

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,37 +3,37 @@ import ReactDOM from "react-dom";
 
 const Header = props => {
   return (
-    <>
+    <div>
       <h1>{props.course}</h1>
-    </>
+    </div>
   );
 };
 
 const Part = props => {
   return (
-    <>
+    <div>
       <p>
         {props.name} {props.exercises}
       </p>
-    </>
+    </div>
   );
 };
 
 const Content = props => {
   return (
-    <>
+    <div>
       <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
       <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
       <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </>
+    </div>
   );
 };
 
 const Total = props => {
   return (
-    <>
+    <div>
       <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    </>
+    </div>
   );
 };
 
